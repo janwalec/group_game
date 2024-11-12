a02330761d4f8fb6ac61ab278559947e0072faa9
Ena Salaj <enasalaj@gmail.com>
2024-11-12 16:56:39 +0100

    adding prefabs and scripts

---
+++ Assets/Scripts/enemies/CaptainShip.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptainShip : EnemyController
+{
+    private bool isSlowed = false;
+    public GameObject pirateShipPrefab;
+    private Vector3 spawnOffset = new Vector3(1f, -1f, 0f);  // Offset to spawn enemies down-right of the CaptainShip
+    private float spawnInterval = 10f;  // Interval to spawn new enemies
+    private float initialDelay = 10f;   // Initial delay before starting the spawn routine
+    private List<Transform> enemies;
+    private EnemyWave wave;
+    private GameObject shipPrefab;
+
+    public void Initialize(EnemyWave wave)
+    {
+        this.wave = wave; // Set the reference to EnemyWave
+        health = 10;
+        this.speed = 1f;
+        Prepare();
+        StartCoroutine(SpawnEnemyRoutine());  // Start spawning minions
+    }
+    protected override IEnumerator SlowDown(float newSpeed)
+    {
+        if (newSpeed >= this.speed)
+        {
+            newSpeed = this.speed;
+        }
+
+        speed = newSpeed;  // Apply slowing effect factor to speed
+        Debug.Log("Enemy speed: " + speed);
+        yield return new WaitForSeconds(1f);  // Slow effect lasts for 3 seconds
+    }
+
+    public override void Move()
+    {
+        base.Move();
+    }
+
+    private IEnumerator SpawnEnemyRoutine()
+    {
+        Debug.Log("SpawnEnemyRoutine coroutine started. Waiting for initial delay...");
+        yield return new WaitForSeconds(initialDelay);
+
+        Debug.Log("Initial delay complete. First ship instantiation starting now.");
+
+        while (health > 0)  // Continue spawning as long as CaptainShip is alive
+        {
+            Debug.Log("Spawning a new pirate ship at time: " + Time.time);
+            SpawnEnemy();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    public int GetHealth()
+    {
+        return base.health;
+    }
+
+    private void SpawnEnemy()
+    {
+        if (pirateShipPrefab != null)
+        {
+            Vector3 spawnPosition = transform.position + spawnOffset;
+            GameObject newPirateShip = Instantiate(pirateShipPrefab, spawnPosition, Quaternion.identity);
+            Transform pirateBoat = newPirateShip.transform.Find("Pirate_Boat");
+            NormalShip normalShip = pirateBoat.GetComponent<NormalShip>();
+
+            if (normalShip != null)
+            {
+                normalShip.InitializeHealth(5);
+            }
+            else
+            {
+                Debug.LogError("NormalShip component not found on the PirateShip prefab.");
+            }
+
+            if (wave != null)
+            {
+                wave.AddEnemy(newPirateShip.transform);
+                Debug.Log("Added PirateShip to EnemyWave's tracking list.");
+            }
+        }
+        else
+        {
+            Debug.LogError("PirateShip prefab is not assigned in the Inspector!");
+        }
+    }
+
+}
---
+++ Assets/Scripts/enemies/Kraken.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kraken : EnemyController
+{
+    private int threshold = 7;
+    private bool canTakeDmg; 
+    private void Start()
+    {
+        this.speed = 1.5f;
+        health = 10;
+        Prepare();
+    }
+
+    public override void Move()
+    {
+
+        base.Move();
+    }
+
+    public void checkDamage(int dmg)
+    {
+        if(dmg<threshold)
+        {
+            canTakeDmg = false;
+        }
+        else
+        {
+            canTakeDmg = true;
+        }
+    }
+
+    public override void TakeDamage(int dmg)
+    {
+        checkDamage(dmg);
+        if(canTakeDmg == true)
+        {
+            base.TakeDamage(dmg);
+        }
+    }
+
+    /* public override void Die()
+     {
+
+         Debug.Log("Normal boat has been destroyed.");
+         base.Die();
+     }*/
+}
